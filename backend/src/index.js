555e04836220b424bffeb553d958f9ce330ca64e
deleonardorabelo <leonardomrabelo@gmail.com>
2020-03-30 20:56:33 -0300

    Celebreate added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { errors } = require('celebrate')
 const routes  = require('./routes')
 const cors    = require('cors')
 const app = express()
@@ -6,5 +7,5 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-
+app.use(errors())
 app.listen(21068)
